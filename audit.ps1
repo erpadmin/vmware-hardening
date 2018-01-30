fb4b589b8afb6b813795a11d2e7235af4388cb78
erpadmin <richie.markham@essent.us>
2018-01-30 10:25:11 -0500

    sweep 2

--- audit.ps1
+++ audit.ps1
@@ -1,150 +1,213 @@
+# version 0
 #
 
-Write-Host "Droping all connections if any"
+Write-Host "`n# ---------- Dropping all connections if any"
 Disconnect-VIServer -Force -server * -Confirm:$false
 
-Write-Host "Setting up prerequistes"
+Write-Host "# ---------- Setting up prerequistes"
 . ./functions.ps1
-set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-Set-PowerCLIConfiguration -DefaultVIServerMode multiple -Confirm:$false
+set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
+Set-PowerCLIConfiguration -DefaultVIServerMode multiple -Confirm:$false | Out-Null
 
-Write-Host "vCenter Server connection details"
+# vCenter Server connection details
 $vCenterServer = "vcsa.lab.local"
 $vCusername = "root"
 $vCpassword = "VMware1!"
 
-Write-Host "common username and password for your ESXi hosts."
+# common username and password for your ESXi hosts
 $esxusername = "root"
-$esxpassword = "VMware1!"
+$esxpassword = $vCpassword
 
-Write-Host "Connect to vCenter"
-$vCconnection = connect-viserver $vCenterServer -user $vCusername -password $vCpassword
+Write-Host "`n# ---------- Connecting to vCenter"
+$vCconnection = connect-viserver $vCenterServer -user $vCusername -password $vCpassword | Out-Null
 $esxihosts = get-vmhost
 $esxivms = get-vm
 
-Write-Host "Audit the list of users who are on the Exception Users List and whether the have administrator privleges"
-foreach ($esxihost in $esxihosts)
-{
-    Write-Host "Host is: " $esxihost
+Write-Host "`n# ---------- Start Audit`n"
+
+# establish direct connections to the esxi hosts
+Connect-VIserver -server $esxihosts -user $esxusername -password $esxpassword
+
+Foreach ($VMHost in $esxihosts) {
+    Write-Host "`n# ----- Checking host $VMHost ...`n"
+    Write-Host "# Audit the list of users who are on the Exception Users List and whether the have administrator privleges"
     Write-host "Exception Users from vCenter"
-    $myhost = Get-VMHost $esxihost | Get-View
+    #$myhost = Get-VMHost $VMHost | Get-View
+    $myhost = $VMHost | Get-View
     $lockdown = Get-View $myhost.ConfigManager.HostAccessManager
     $LDusers = $lockdown.QueryLockdownExceptions()
     Write-host $LDusers
-}
 
-
-Write-Host "Check to see if the SSH Server Is running"
-foreach ($VMhost in (Get-VMHost))
-{
+    Write-Host "# Check to see if the SSH Server Is running"
     $ServiceList = Get-VMHostService -VMhost $VMhost
     $SSHservice = $ServiceList | Where-Object {$_.Key -eq "TSM-SSH"}
+
     If ($SSHservice.Running -eq $true) {
         Write-Output "SSH Server on host $VMhost is running"
     }
     else {
         Write-Output "SSH Server on host $VMhost is Stopped"
     }
-}
 
+    Write-Host "# Check the NTP Setting"
+    $VMHost | Select @{N="NTP";E={$_ | Get-VMHostNtpServer}}
 
-Write-Host "List the NTP Settings for all hosts "
-Get-VMHost | Select Name, @{N="NTPSetting";E={$_ | Get-VMHostNtpServer}}
+    Write-Host "# Check Syslog.global.logDir"
+    $VMHost | Select @{N="Syslog.global.logDir";E={$_ | Get-AdvancedSetting -Name Syslog.global.logDir | Select -ExpandProperty Values}}
 
-Write-Host "List Syslog.global.logDir for each host"
-Get-VMHost | Select Name, @{N="Syslog.global.logDir";E={$_ | Get-VMHostAdvancedConfiguration Syslog.global.logDir | Select -ExpandProperty Values}}
+    Write-Host "# Check the SNMP Configuration"
+    #$VMHost | Get-VMHostSnmp
+    Write-Host "this check is erroring out, disabling it for now"
 
-# List the SNMP Configuration of a host (single host connection required)
-# will have to add some connect code for the hosts before this call works
-#Get-VMHost | Get-VMHostSnmp
+    Write-Host "# Check Managed Object Browser (MOB)"
+    $VMHost | Get-AdvancedSetting -Name Config.HostAgent.plugins.solo.enableMob | Select Name, Value
 
-Write-Host "Check Managed Object Browser (MOB)"
-Get-VMHost | Get-AdvancedSetting -Name Config.HostAgent.plugins.solo.enableMob
+    Write-Host "# Disable TLS 1.0 and 1.1 on ESXi Hosts if necessary"
+    $VMHost | Get-AdvancedSetting -Name UserVars.ESXiVPsDisabledProtocols | Select Name, Value
 
-Write-Host "Disable TLS 1.0 and 1.1 on ESXi Hosts if necessary"
-Get-VMHost | Get-AdvancedSetting -Name UserVars.ESXiVPsDisabledProtocols
+    Write-Host "# Check each host and their domain membership status"
+    $VMHost | Get-VMHostAuthentication | Select VmHost, Domain, DomainMembershipStatus
 
-Write-Host "Check each host and their domain membership status"
-Get-VMHost | Get-VMHostAuthentication | Select VmHost, Domain, DomainMembershipStatus
+    Write-Host "# Check the host profile is using vSphere Authentication proxy to add the host to the domain"
+    $VMHost | Select ` @{N="HostProfile";E={$_ | Get-VMHostProfile}}, ` @{N="JoinADEnabled";E={($_ | Get-VmHostProfile).ExtensionData.Config.ApplyProfile.Authentication.ActiveDirectory.Enabled}}, ` @{N="JoinDomainMethod";E={(($_ | Get-VMHostProfile).ExtensionData.Config.ApplyProfile.Authentication.ActiveDirectory | Select -ExpandProperty Policy | Where {$_.Id -eq "JoinDomainMethodPolicy"}).Policyoption.Id}}
 
+    Write-Host "# List Iscsi Initiator and CHAP Name if defined"
+    $VMHost | Get-VMHostHba | Where {$_.Type -eq "Iscsi"} | Select VMHost, Device, ChapType, @{N="CHAPName";E={$_.AuthenticationProperties.ChapName}}
 
-Write-Host "Check the host profile is using vSphere Authentication proxy to add the host to the domain"
-Get-VMHost | Select Name, ` @{N="HostProfile";E={$_ | Get-VMHostProfile}}, ` @{N="JoinADEnabled";E={($_ | Get-VmHostProfile).ExtensionData.Config.ApplyProfile.Authentication.ActiveDirectory.Enabled}}, ` @{N="JoinDomainMethod";E={(($_ | Get-VMHostProfile).ExtensionData.Config.ApplyProfile.Authentication.ActiveDirectory | Select -ExpandProperty Policy | Where {$_.Id -eq "JoinDomainMethodPolicy"}).Policyoption.Id}}
+    Write-Host "# Check if Lockdown mode is enabled"
+    $VMHost | Select @{N="Lockdown";E={$_.Extensiondata.Config.adminDisabled}}
 
-Write-Host "List Iscsi Initiator and CHAP Name if defined"
-Get-VMHost | Get-VMHostHba | Where {$_.Type -eq "Iscsi"} | Select VMHost, Device, ChapType, @{N="CHAPName";E={$_.AuthenticationProperties.ChapName}}
+    Write-Host "# Check remote logging for ESXi hosts "
+    $VMHost | Get-AdvancedSetting -Name Syslog.global.logHost | Select Name, Value
 
-Write-Host "Check if Lockdown mode is enabled"
-Get-VMHost | Select Name,@{N="Lockdown";E={$_.Extensiondata.Config.adminDisabled}}
+    Write-Host "# Check if Lockdown mode is enabled"
+    $VMHost | Select @{N="Lockdown";E={$_.Extensiondata.Config.adminDisabled}}
 
-Write-Host "Check remote logging for ESXi hosts "
-Get-VMHost | Get-AdvancedSetting -Name Syslog.global.logHost
-
-Write-Host "Check if Lockdown mode is enabled"
-Get-VMHost | Select Name,@{N="Lockdown";E={$_.Extensiondata.Config.adminDisabled}}
-
-Write-Host "To display the mode "
-foreach ($esxihost in $esxihosts)
-{
-    $myhost = Get-VMHost $esxihost | Get-View
+    Write-Host "# Check Lockdown setting"
+    $myhost = $VMHost | Get-View
     $lockdown = Get-View $myhost.ConfigManager.HostAccessManager
-    Write-Host "-----------"
     $lockdown.UpdateViewData()
     $lockdownstatus = $lockdown.LockdownMode
-    Write-Host "Lockdown mode on $esxihost is set to $lockdownstatus"
-    Write-Host "-----------"
+    Write-Host "Lockdown mode is set to $lockdownstatus"
+
+    Write-Host "`n# List the services which are enabled and have rules defined for specific IP ranges to access the service"
+    $withrules = $VMHost | Get-VMHostFirewallException | Where {$_.Enabled -and (-not $_.ExtensionData.AllowedHosts.AllIP)}
+    Write-Host $withrules
+    Write-Host $withrules.extensiondata.allowedhosts
+
+    Write-Host "`n# List the services which are enabled and do not have rules defined for specific IP ranges to access the service"
+    $withoutrules = $VMHost | Get-VMHostFirewallException | Where {$_.Enabled -and ($_.ExtensionData.AllowedHosts.AllIP)}
+    Write-Host $withoutrules
+    Write-Host $withoutrules.extensiondata.allowedhosts
+
+    Write-Host "`n# Set the time after which a locked account is automatically unlocked"
+    $VMHost | Get-AdvancedSetting -Name Security.AccountUnlockTime | Select Name, Value
+
+    Write-Host "# Set the count of maximum failed login attempts before the account is locked out"
+    $VMHost | Get-AdvancedSetting -Name Security.AccountLockFailures | Select Name, Value
+
+    Write-Host "# Set DCUI.Access to allow trusted users to override lockdown mode"
+    $VMHost | Get-AdvancedSetting -Name DCUI.Access | Select Name, Value
+
+    Write-Host "# Audit DCUI timeout value"
+    $VMHost | Get-AdvancedSetting -Name UserVars.DcuiTimeOut | Select Name, Value
+
+    Write-Host "# Establish a password policy for password complexity"
+    $VMHost | Get-AdvancedSetting -Name Security.PasswordQualityControl | Select Name, Value
+
+    Write-Host "# Set a timeout to automatically terminate idle ESXi Shell and SSH sessions"
+    $VMHost | Get-AdvancedSetting -Name UserVars.ESXiShellInteractiveTimeOut | Select Name, Value
+
+    Write-Host "# Set a timeout to limit how long the ESXi Shell and SSH services are allowed to run"
+    $VMHost | Get-AdvancedSetting -Name UserVars.ESXiShellTimeOut | Select Name, Value
+
+    Write-Host "# Ensure default setting for intra-VM TPS is correct"
+    $VMHost | Get-AdvancedSetting -Name Mem.ShareForceSalting | Select Name, Value
+
+    Write-Host "# List the Software AcceptanceLevel for each host"
+    $ESXCli = Get-EsxCli -VMHost $VMHost | Select @{N="AcceptanceLevel";E={$ESXCli.software.acceptance.get()}}
+    Write-Host $ESXCli
+
+    Write-Host "`n# List only the vibs which are not at "VMwareCertified" or "VMwareAccepted" or "PartnerSupported" acceptance level"
+    #$ESXCli = Get-EsxCli -VMHost $VMHost $ESXCli.software.vib.list() |
+    #        Where { ($_.AcceptanceLevel -ne "VMwareCertified") -and ($_.AcceptanceLevel -ne "VMwareAccepted")
+    #        -and ($_.AcceptanceLevel -ne "PartnerSupported") }
+    Write-Host "this check is erroring out, disabling it for now"
+
+    Write-Host "# BPDU filter (prevents being locked out of physical switch ports with Portfast and BPDU Guard enabled)"
+    $VMHost | Get-AdvancedSetting -Name Net.BlockGuestBPDU | Select Name, Value
+
+    Write-Host "# Audit use of dvfilter network APIs"
+    $VMHost | Get-AdvancedSetting -Name Net.DVFilterBindIpAddress | Select Name, Value
+
+    ##### Networking check subsection
+    Write-Host "# Ensure that the Forged Transmits policy is set to reject"
+    Write-Host "# Ensure that the MAC Address Changes policy is set to reject"
+    Write-Host "# Ensure that the Promiscuous Mode policy is set to reject"
+
+    foreach ($vSwitch in $VMHost | Get-VirtualSwitch -Standard) {
+        Write-Host " "$vSwitch.Name
+        Write-Host "`tPromiscuous mode enabled:" $vSwitch.ExtensionData.Spec.Policy.Security.AllowPromiscuous
+        Write-Host "`tForged transmits enabled:" $vSwitch.ExtensionData.Spec.Policy.Security.ForgedTransmits
+        Write-Host "`tMAC Changes enabled:" $vSwitch.ExtensionData.Spec.Policy.Security.MacChanges
+
+        foreach($portgroup in ($VMHost.ExtensionData.Config.Network.Portgroup | where {$_.Vswitch -eq $vSwitch.Key})){
+            Write-Host "`n`t`t"$portgroup.Spec.Name
+            Write-Host "`t`t`tPromiscuous mode enabled: " -nonewline
+
+            If ($portgroup.Spec.Policy.Security.AllowPromiscuous -eq $null) {
+                Write-Host $vSwitch.ExtensionData.Spec.Policy.Security.AllowPromiscuous
+            } Else {
+                Write-Host $portgroup.Spec.Policy.Security.AllowPromiscuous
+            }
+
+            Write-Host "`t`t`tForged transmits enabled: " -nonewline
+
+            If ($portgroup.Spec.Policy.Security.ForgedTransmits -eq $null) {
+                Write-Host $vSwitch.ExtensionData.Spec.Policy.Security.ForgedTransmits
+            } Else {
+                Write-Host $portgroup.Spec.Policy.Security.ForgedTransmits
+            }
+
+            Write-Host "`t`t`tMAC Changes enabled: " -nonewline
+
+            If ($portgroup.Spec.Policy.Security.MacChanges -eq $null) {
+                Write-Host $vSwitch.ExtensionData.Spec.Policy.Security.MacChanges
+            } Else {
+                Write-Host $portgroup.Spec.Policy.Security.MacChanges
+            }
+        }
+    }
+
+    foreach ($vSwitch in $esxihosts | Get-VirtualSwitch -Distributed) {
+        Write-Host " "$vSwitch.Name
+        Write-Host "`tPromiscuous mode enabled:" $vSwitch.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.AllowPromiscuous.Value
+        Write-Host "`tForged transmits enabled:" $vSwitch.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.ForgedTransmits.Value
+        Write-Host "`tMAC Changes enabled:" $vSwitch.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.MacChanges.Value
+
+        foreach($portgroup in (Get-VirtualPortGroup -Distributed -VirtualSwitch $vSwitch)){
+            Write-Host "`n`t`t"$portgroup.Name
+            Write-Host "`t`t`tPromiscuous mode enabled:" $portgroup.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.AllowPromiscuous.Value
+            Write-Host "`t`t`tForged transmits enabled:" $portgroup.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.ForgedTransmits.Value
+            Write-Host "`t`t`tMAC Changes enabled:" $portgroup.Extensiondata.Config.DefaultPortConfig.SecurityPolicy.MacChanges.Value
+
+            #remove this later: Get-VDPortgroup | Select Name, VirtualSwitch, @{Name="NetflowEnabled";Expression={$_.Extensiondata.Config.defaultPortConfig.ipfixEnabled.Value}} | Where-Object {$_.NetflowEnabled -eq "True"}
+            Write-Host "`t`t`tVDS Netflow traffic is only being sent to authorized collector IPs:" $portgroup.Extensiondata.Config.DefaultPortConfig.ipfixEnabled.Value
+
+            Write-Host "Check port-level configuration overrides on VDS"
+            $portgroup | Get-VDPortgroupOverridePolicy
+        }
+    }
+
+    Write-Host "# Check VDS network healthcheck"
+    $vds = Get-VDSwitch
+    $vds.ExtensionData.Config.HealthCheckConfig
+    Get-View -ViewType DistributedVirtualSwitch
 }
 
-Write-Host "List the services which are enabled and have rules defined for specific IP ranges to access the service"
-Get-VMHost  | Get-VMHostFirewallException | Where {$_.Enabled -and (-not $_.ExtensionData.AllowedHosts.AllIP)}
+exit
 
-Write-Host "List the services which are enabled and do not have rules defined for specific IP ranges to access the service"
-Get-VMHost  | Get-VMHostFirewallException | Where {$_.Enabled -and ($_.ExtensionData.AllowedHosts.AllIP)}
-
-Write-Host "Set the time after which a locked account is automatically unlocked"
-Get-VMHost | Get-AdvancedSetting -Name Security.AccountUnlockTime
-
-Write-Host "Set the count of maximum failed login attempts before the account is locked out"
-Get-VMHost | Get-AdvancedSetting -Name Security.AccountLockFailures
-
-Write-Host "Set DCUI.Access to allow trusted users to override lockdown mode"
-Get-VMHost | Get-AdvancedSetting -Name DCUI.Access
-
-Write-Host "Audit DCUI timeout value"
-Get-VMHost | Get-AdvancedSetting -Name UserVars.DcuiTimeOut
-
-Write-Host "Establish a password policy for password complexity"
-Get-VMHost | Get-AdvancedSetting -Name Security.PasswordQualityControl
-
-Write-Host "Set a timeout to automatically terminate idle ESXi Shell and SSH sessions"
-Get-VMHost | Get-AdvancedSetting -Name UserVars.ESXiShellInteractiveTimeOut
-
-Write-Host "Set a timeout to limit how long the ESXi Shell and SSH services are allowed to run"
-Get-VMHost | Get-AdvancedSetting -Name UserVars.ESXiShellTimeOut
-
-Write-Host "Ensure default setting for intra-VM TPS is correct"
-Get-VMHost | Get-AdvancedSetting -Name Mem.ShareForceSalting
-
-# establish real direct connections to the hosts
-Connect-VIserver -server $esxihosts -user $esxusername -password $esxpassword
-
-
-Write-Host "List the Software AcceptanceLevel for each host"
-Foreach ($VMHost in $esxihosts) {
-    $ESXCli = Get-EsxCli -VMHost $VMHost | Select Name, @{N="AcceptanceLevel";E={$ESXCli.software.acceptance.get()}}
-    Wite-Host $ESXCli
-}
-
-# List only the vibs which are not at "VMwareCertified" or "VMwareAccepted" or "PartnerSupported" acceptance level
-#Foreach ($VMHost in Get-VMHost ) {
-#    $ESXCli = Get-EsxCli -VMHost $VMHost $ESXCli.software.vib.list() | \
-#        Where { ($_.AcceptanceLevel -ne "VMwareCertified") -and ($_.AcceptanceLevel -ne "VMwareAccepted") \
-#        -and ($_.AcceptanceLevel -ne "PartnerSupported") }
-#}
-
-# hack, drop vcenter connection 
-#Disconnect-VIServer -Force -server $vCenterServer -Confirm:$false
-
+##### VM Check Section
 Foreach ($VM in $esxivms) {
     Write-Host "- Checking $VM ..."
     Write-Host "isolation.tools.copy.disable"
@@ -202,46 +265,3 @@ Foreach ($VM in $esxivms) {
     Get-AdvancedSetting -Entity $VM -Name  "ethernet*.filter*.name*" | Select Entity, Name, Value
     Get-AdvancedSetting -Entity $VM -Name "pciPassthru*.present" | Select Entity, Name, Value
 }
-
-Write-Host "Enable BPDU filter on the ESXi host to prevent being locked out of physical switch ports with Portfast and BPDU Guard enabled"
-Get-VMHost | Get-AdvancedSetting -Name Net.BlockGuestBPDU
-
-# convert this into a check
-# Enable VDS network healthcheck only if you need it
-#$vds = Get-VDSwitch
-#$vds.ExtensionData.Config.HealthCheckConfig
-#Get-View -ViewType DistributedVirtualSwitch | ?{($_.config.HealthCheckConfig | ?{$_.enable -notmatch "False"})}| %{$_.UpdateDVSHealthCheckConfig(@((New-Object Vmware.Vim.VMwareDVSVlanMtuHealthCheckConfig -property @{enable=0}),(New-Object Vmware.Vim.VMwareDVSTeamingHealthCheckConfig -property @{enable=0})))}
-
-Write-Host "Ensure that the Forged Transmits policy is set to reject"
-Get-VDSwitch | Get-VDSecurityPolicy
-Get-VDPortgroup | ?{$_.IsUplink -eq $false} | Get-VDSecurityPolicy
-
-Write-Host "Ensure that the Forged Transmits policy is set to reject"
-Get-VirtualSwitch | Get-SecurityPolicy
-Get-VirtualPortGroup | Get-SecurityPolicy
-
-Write-Host "Ensure that the MAC Address Changes policy is set to reject"
-Get-VDSwitch | Get-VDSecurityPolicy
-Get-VDPortgroup | Get-VDSecurityPolicy
-
-Write-Host "Ensure that the MAC Address Changes policy is set to reject"
-Get-VirtualSwitch | Get-SecurityPolicy
-Get-VirtualPortGroup | Get-SecurityPolicy
-
-Write-Host "Ensure that the Promiscuous Mode policy is set to reject"
-Get-VDSwitch | Get-VDSecurityPolicy
-Get-VDPortgroup | Get-VDSecurityPolicy
-
-Write-Host "Ensure that the Promiscuous Mode policy is set to reject"
-Get-VirtualSwitch | Get-SecurityPolicy
-Get-VirtualPortGroup | Get-SecurityPolicy
-
-Write-Host "Ensure that VDS Netflow traffic is only being sent to authorized collector IPs"
-Get-VDPortgroup | Select Name, VirtualSwitch, @{Name="NetflowEnabled";Expression={$_.Extensiondata.Config.defaultPortConfig.ipfixEnabled.Value}} | Where-Object {$_.NetflowEnabled -eq "True"}
-
-Write-Host "Restrict port-level configuration overrides on VDS "
-Get-VDPortgroup | Get-VDPortgroupOverridePolicy
-
-Write-Host "Audit use of dvfilter network APIs"
-Get-VMHost | Get-AdvancedSetting -Name Net.DVFilterBindIpAddress
-
